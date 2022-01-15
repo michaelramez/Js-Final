52f8510183befdf46eb6cc69fc10ba915ef1e775
Michael Ramez <34134110+michaelramez@users.noreply.github.com>
2022-01-15 20:07:05 +0200

    Last-Upload

--- high scores/highScores.js
+++ high scores/highScores.js
@@ -1,31 +1,31 @@
 
 
-  let div = document.getElementById("div_highScores");
+let div = document.getElementById("div_highScores");
 
-  if (localStorage !== null) {
+if (localStorage !== null) {
 
-    let arrHighScore=[{key:'A',value:0}];
+  let arrHighScore = [{ key: 'A', value: 0 }];
 
-      for (var i=0; i < localStorage.length; i++)  {
-          arrHighScore[i]={
-          key:localStorage.key(i),
-          value:localStorage.getItem(localStorage.key(i))
-        }
-        // console.log(localStorage.key(i));
-        // arrHighScore[i].key=localStorage.key(i);
-        // arrHighScore[i].value=localStorage.getItem(localStorage.key(i));
-     }
-   
-    arrHighScore.sort(function(a, b){return b.value - a.value});
-    
-    console.log(arrHighScore);
-    
-    for (var i=0; i < localStorage.length; i++)  {
-      
-        div.innerHTML += '<br><p> '+arrHighScore[i].key+': '+arrHighScore[i].value+' </p>';
-        
-     }
-   }
+  for (var i = 0; i < localStorage.length; i++) {
+    arrHighScore[i] = {
+      key: localStorage.key(i),
+      value: localStorage.getItem(localStorage.key(i))
+    }
+    // console.log(localStorage.key(i));
+    // arrHighScore[i].key=localStorage.key(i);
+    // arrHighScore[i].value=localStorage.getItem(localStorage.key(i));
+  }
 
-   
-  
+  arrHighScore.sort(function (a, b) { return b.value - a.value });
+
+  console.log(arrHighScore);
+
+  for (var i = 0; i < localStorage.length; i++) {
+    let num = i + 1;
+    div.innerHTML += '<br><p>' + num + '-' + arrHighScore[i].key + ': ' + arrHighScore[i].value + ' </p>';
+
+  }
+}
+
+
+
